796317a11ff2e85d6c9d9dee025c910c1e515560
Oleksandr Savych <osavych@stellar.aero>
2020-05-27 22:30:30 +0300

    Use `express-session` to work with sessions Use `connect-mongodb-session` to store sessions in DB

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ const expressHandlebars = require('express-handlebars');
 const Handlebars = require('handlebars');
 const { allowInsecurePrototypeAccess } = require('@handlebars/allow-prototype-access');
 const mongoose = require('mongoose');
+const expressSession = require('express-session');
+const MongoStore = require('connect-mongodb-session')(expressSession);
 
 const homeRoutes = require('./routes/home');
 const cartRoutes = require('./routes/cart');
@@ -12,7 +14,12 @@ const coursesRoutes = require('./routes/courses');
 const ordersRoutes = require('./routes/orders');
 const authRoutes = require('./routes/auth');
 
-const User = require('./models/user');
+const variablesMiddleware = require('./middleware/variables');
+const mongoDBUrl = 'mongodb+srv://course-market-user:xmCfucpUGvivKEsx@cluster0-zxqn8.mongodb.net/courseMarket';
+const sessionStore = new MongoStore({
+  collection: 'sessions',
+  uri: mongoDBUrl
+});
 
 const app = express();
 
@@ -24,18 +31,16 @@ app.engine('hbs', expressHandlebars({
 app.set('view engine', 'hbs');
 app.set('views', 'views');
 
-app.use(async (req, res, next) => {
-  try {
-    const user = await User.findById('5ece13d8affab42a44ca6830');
-    req.user = user;
-    next();
-  } catch (e) {
-    console.error(e);
-  }
-});
-
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.urlencoded({extended: true}));
+
+app.use(expressSession({
+  secret: 'some secret value',
+  resave: false,
+  saveUninitialized: false,
+  store: sessionStore
+}));
+app.use(variablesMiddleware);
 
 app.use('/', homeRoutes);
 app.use('/add', addRoutes);
@@ -46,23 +51,11 @@ app.use('/auth', authRoutes);
 
 async function start() {
   try {
-    const mongoDBUrl = 'mongodb+srv://course-market-user:xmCfucpUGvivKEsx@cluster0-zxqn8.mongodb.net/courseMarket';
     await mongoose.connect(mongoDBUrl, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
       useFindAndModify: false
     });
-
-    const userCandidate = await User.findOne();
-    if (!userCandidate) {
-      const user = new User({
-        email: 'osavych@gmail.com',
-        name: 'Sasha',
-        cart: { items: [] }
-      });
-
-      await user.save();
-    }
 
     const PORT = process.env.PORT || 3000;
     app.listen(PORT, () => {
--- routes/auth.js
+++ routes/auth.js
@@ -1,10 +1,31 @@
 const { Router } = require('express');
 const router = Router();
 
+const User = require('../models/user');
+
 router.get('/login', async (req, res) => {
    res.render('auth/login', {
       title: 'Authorization',
       isLogin: true
+   });
+});
+
+router.post('/login', async (req, res) => {
+   req.session.user = await User.findById('5ece13d8affab42a44ca6830');
+   req.session.isAuthenticated = true;
+
+   req.session.save(err => {
+      if (err) {
+         throw err;
+      } else {
+         res.redirect('/');
+      }
+   });
+});
+
+router.get('/logout', async (req, res) => {
+   req.session.destroy(() => {
+      res.redirect('/auth/login#login');
    });
 });
 
